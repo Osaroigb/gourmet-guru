49a632c92b2956d3c63b60d57abc71d1c523ecec
Osaro Igbinovia <emmyigbinovia@gmail.com>
2024-02-02 06:24:53 +0100

    feat: postgres database setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        // entities: [Restuarant]
+        entities: [join(process.cwd(), 'dist/**/*.entity.js')]
+      })
+    }),
+    RestaurantsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
